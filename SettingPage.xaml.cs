8d051ff72c412f1922605bdc8e8defd38d640440
moxitech <moxitogame59@mail.com>
2022-09-17 12:14:44 +0700

    Обновление настроек

--- SettingPage.xaml.cs
+++ SettingPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace Moxitech_Tasklist_Open_Source;
 
 public partial class SettingPage : ContentPage
@@ -10,5 +12,28 @@ public partial class SettingPage : ContentPage
 	private async void Back_Button_Clicked(object sender, EventArgs e)
 	{
 		await Shell.Current.GoToAsync("///MainPage");
+	}
+
+	private void Color_Change_Picker(object sender, EventArgs e)
+	{
+		string color = ColorPicker.SelectedItem.ToString();
+		switch (color)
+		{
+			case "Темная тема":
+				
+				break;
+
+            case "Светлая тема":
+                break;
+
+            case "Летняя тема":
+                break;
+
+            case "Зимняя тема":
+                break;
+			default:
+
+				break;
+        }
 	}
 }
